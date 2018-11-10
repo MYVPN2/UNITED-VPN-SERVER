f4607e5bdd7f563d4ff901faa561ad1c377e3210
MYVPN2 <44933962+MYVPN2@users.noreply.github.com>
2018-11-11 07:58:47 +0800

    Add files via upload

---
+++ payload.js
@@ -0,0 +1,105 @@
+//Edit nyo lang ang payload then sa Url example m.facebook.com eencrypt nyo muna bago i-repalce
+[
+  {
+	  "NAME":"GTM-G0SURF 1",
+	  "Info":"Register to GOSURF",
+	  "Payload":{
+		  "Proxy":{
+			  "Custom":false,
+			  "Remote":"178.128.63.127",
+			  "Port":"8080"
+		  },
+		  "Url":"AA9FAA9FAA9FD6FA0CFAAAFAAAFA0CFA08FA0AFD6F99FAAAFA09F",
+		  "Host":true,
+		  "Online-Host":true,
+		  "Forward-Host":true,
+		  "Reverse-Proxy":false,
+		  "Keep-Alive":true
+	  }
+  },
+  {
+	  "NAME":"GTM-GOSURF 2",
+	  "Info":"Register to GO SURF",
+	   "Payload":{
+		  "Proxy":{
+			  "Custom":false,
+			  "Remote":"209.97.162.80",
+			  "Port":"8080"
+		  },
+		  "Url":"AA9FAA9FAA9FD6FA0CFAAAFAAAFA0CFA08FA0AFD6F99FAAAFA09F",
+		  "Host":true,
+		  "Online-Host":true,
+		  "Forward-Host":true,
+		  "Reverse-Proxy":false,
+		  "Keep-Alive":true
+	  }
+  },
+  {
+	  "NAME":"GTM-GOSURF 3",
+	  "Info":"Register to GO SURF",
+	   "Payload":{
+		  "Proxy":{
+			  "Custom":true,
+			  "Remote":"178.128.83.204",
+			  "Port":"8080"
+		  },
+		  "Url":"AA9FAA9FAA9FD6FA0CFAAAFAAAFA0CFA08FA0AFD6F99FAAAFA09F",
+		  "Host":true,
+		  "Online-Host":true,
+		  "Forward-Host":true,
+		  "Reverse-Proxy":false,
+		  "Keep-Alive":true
+	  }
+  },
+    {
+	  "NAME":"SUN-SERVER 1",
+	  "Info":"Register to TU50",
+	   "Payload":{
+		  "Proxy":{
+			  "Custom":true,
+			  "Remote":"178.128.63.127",
+			  "Port":"8080"
+		  },
+		  "Url":"AA9FA0AFA0EFAB0FA0EFAA0FD6FAACFAACFD6F99FAAAFA09F",
+		  "Host":true,
+		  "Online-Host":true,
+		  "Forward-Host":true,
+		  "Reverse-Proxy":false,
+		  "Keep-Alive":true
+	  }
+  },
+    {
+	  "NAME":"SUN-UNITED-3",
+	  "Info":"Register to TU50",
+	   "Payload":{
+		  "Proxy":{
+			  "Custom":true,
+			  "Remote":"209.97.162.80",
+			  "Port":"8080"
+		  },
+		  "Url":"AA9FAA9FAA9FDEF99FA00FAA0FD6F87FA0DF97FAA6FAAEF6EFAABFAABFD6FAA0FA0AFAA6F",
+		  "Host":true,
+		  "Online-Host":true,
+		  "Forward-Host":true,
+		  "Reverse-Proxy":false,
+		  "Keep-Alive":true
+	  }
+  },
+    {
+	  "NAME":"SUN-UNITED-4",
+	  "Info":"Register to TU50",
+	   "Payload":{
+		  "Proxy":{
+			  "Custom":true,
+			  "Remote":"178.128.83.204",
+			  "Port":"8080"
+		  },
+		  "Url":"AA9FAA9FAA9FDEF99FA00FAA0FD6F87FA0DF97FAA6FAAEF6EFAABFAABFD6FAA0FA0AFAA6F",
+		  "Host":true,
+		  "Online-Host":true,
+		  "Forward-Host":true,
+		  "Reverse-Proxy":false,
+		  "Keep-Alive":true
+	  }
+  }
+]
